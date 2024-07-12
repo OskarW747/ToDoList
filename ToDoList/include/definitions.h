207acbe1068b6a67b5ca2bb3596e0ecaab0dc965
OskarW747 <oskagn@outlook.com>
2024-07-12 03:43:44 +0200

    Coded wrong application at 03:43 hahah

--- ToDoList/include/definitions.h
+++ ToDoList/include/definitions.h
@@ -4,6 +4,16 @@
 /* 
  * Defines used across the program 
  */
-#define MAIN_MENU		0
+
+// Menu indexes
+#define MAIN_MENU_INDEX		0
+
+//MainMenu options
+#define MAIN_DEPOSIT		1
+#define MAIN_WITHDRAW		2
+#define MAIN_EXIT			3
+
+// Customizations 
+#define MENU_WIDTH		30
 
 #endif
--- ToDoList/src/main.cpp
+++ ToDoList/src/main.cpp
@@ -1,9 +1,57 @@
 #include <iostream>
-#include "../include/definitions.h"
+#include <string>
+#include "../include/Definitions.h"
+#include "../include/Menus.h"
 
-int currentMenu = MAIN_MENU;
+using namespace std;
+
+int menuIndex = MAIN_MENU_INDEX;
+bool mainLoop = true;
+
+MainMenu mainMenu;
+
+void printMenu(Menu *_menu) {
+	_menu->displayMenu();
+}
+
+void menuController() {
+	switch (menuIndex) {
+	case(MAIN_MENU_INDEX):
+		printMenu(&mainMenu);
+
+		// get input
+		string _input;
+		int _inputInt;
+		getline(cin, _input);
+
+
+		try {
+			_inputInt = stoi(_input);
+		}
+		catch (invalid_argument e) {
+			cout << "Invalid input";
+		}
+
+		switch (_inputInt) {
+		case(MAIN_DEPOSIT):
+
+		}
+	}
+}
 
 int main() {
-	std::cout << "Hello World" << std::endl;
+	cout << "Hello World" << std::endl;
+
+	string input;
+
+	printMenu(&mainMenu);
+
+	while (mainLoop) {
+		
+		menuController();
+
+	}
+	return 0;
+
 
 }
